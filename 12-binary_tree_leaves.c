231181a29fa59126b27e10f1bb8f3d8eca326c34
Code Slayer <3613@holbertonschool.com>
2022-08-01 16:03:51 -0500

    up to 14

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,14 +1,13 @@
 #include "binary_trees.h"
 
 size_t binary_tree_leaves(const binary_tree_t *tree) {
-    if (tree == NULL) {
-        return 0;
+    size_t count = 0;
+	if (tree == NULL)
+		return 0;
+    if (tree->left == NULL && tree->right == NULL){
+        count++;
     }
-    if (!tree->left && !tree->right){
-        return 1 + binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
-    }
-    return 0;
-
+    return count + binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
 
 
 
