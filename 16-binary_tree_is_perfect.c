b897209ba803f5772b08138631f8985941770b77
Code Slayer <3613@holbertonschool.com>
2022-08-02 11:14:50 -0500

    task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+int binary_tree_is_perfect(const binary_tree_t *tree) {
+    int d;
+	if (tree == NULL)
+		return 0;
+
+    d = depth(tree);
+
+    return binary_tree_is_perfect_rec(tree, d, 0) ;
+
+
+}
+int binary_tree_is_perfect_rec(const binary_tree_t *tree, int d, int level) {
+	if (tree == NULL)
+		return 1;
+
+    if (tree->left == NULL && tree->right == NULL) {
+        return (d = level + 1);
+    }
+    if (tree->left == NULL || tree->right == NULL) {
+        return 0;
+    }
+
+    return binary_tree_is_perfect_rec(tree->left, d, level+1) && binary_tree_is_perfect_rec(tree->right, d, level+1);
+
+
+}
+
+int depth(const binary_tree_t *tree) {
+    int d = 0;
+    while (tree != NULL){
+        d++;
+        tree = tree->left;
+    }
+    return d;
+
+}
